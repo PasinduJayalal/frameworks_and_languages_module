3c536f6118ddc8e524cb7ab80de7a8f6300c5957
Pasindu Jayalal <95409421+PasinduJayalal@users.noreply.github.com>
2023-10-09 10:24:32 +0000

    Sessions Examples

---
+++ expresstest/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    res.json({"foo": "bar"})
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+process.on('SIGINT', function() {process.exit()})
