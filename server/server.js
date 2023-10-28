d1bbc2661e0d8cb6e1c08a4d18ca720c5bab4ff2
Pasindu Jayalal <95409421+PasinduJayalal@users.noreply.github.com>
2023-10-28 17:42:58 +0000

    Express Server Basic 2

--- server/server.js
+++ server/server.js
@@ -9,10 +9,17 @@ app.use(cors())
 let ITEMS=[{
   "id": 0,
   "user_id": "user1234",
-  "keywords": ["hammer","nails","tools"],
+  "keywords": [
+    "hammer",
+    "nails",
+    "tools"
+  ],
   "description": "A hammer and nails set",
+  "image": "https://placekitten.com/200/300",
   "lat": 51.2798438,
   "lon": 1.0830275,
+  "date_from": "2023-10-25T21:39:22.733Z",
+  "date_to": "2023-10-25T21:39:22.733Z"
 }]
 
 app.get('/items', (req, res) => {
@@ -20,18 +27,27 @@ app.get('/items', (req, res) => {
 })
 
 app.post('/item/', (req, res) => {
-  if (Object.keys(req.body).toString() != "id,user_id,keywords,description,lat,lon") {
+  if (Object.keys(req.body).toString() != "user_id,keywords,description,lat,lon") {
     return res.status(405).json({message: 'missing fields'})
   }
   const date = new Date().toISOString()
   req.body['Date From'] = date
+  req.body.id = Math.random()
   ITEMS.push(req.body)
   res.status(201).json(req.body)
+})
+app.get('/item/:id', (req, res) => {
+   const one = ITEMS.filter((item) => item.id == req.params.id)
+  res.status(200).json(one);
 })
 
 app.delete('/item/:id', (req, res) => {
   ITEMS = ITEMS.filter((item) => item.id != req.params.id)
   res.status(204).json();
+  if(!ff){
+    console.log("${id} not found")
+    return res.status(404).json();
+  }
 })
 
 app.listen(port, () => {
